ee52e63c7d2871f99dfefe91ea54d164ad29d01a
magyro <rosmanp@bezeqint.net>
2015-06-06 19:04:57 +0300

    Create plot2.r
    working directory should be set by editing the file prior to execution

---
+++ plot2.r
@@ -0,0 +1,64 @@
+## Plot 2 
+## Line graph 
+  
+rm(list=ls())
+
+#####################################################################################
+## if run in different environment it should be set in next sentence
+####################################################################################
+
+setwd("~/Coursera R/Exploratory/W1")
+options(stringsAsFactors = FALSE)
+
+library(data.table)
+library(datasets)
+
+####################################################################################
+## read the data into workData table
+# WD is a file created by subsetting the input file to the reqiured data 
+# 1. If first time, read the input file, create a new file (WD) with the 
+# required data and use it for workData table
+# 2. If WD file exists (not first time), read it into workData table 
+#-----------------------------------------------------------------------------------
+
+if (file.exists("WD.txt")) {
+    workData<-read.table ("WD.txt" ,head=TRUE )
+}else {
+    hpcData <- 
+        read.table ("household_power_consumption.txt",na.strings ="?" ,sep=";",head=TRUE )
+    
+    workData <- 
+        subset(hpcData, strptime(hpcData$Date, "%d/%m/%Y", tz="")==
+                   strptime("2007/02/01", "%Y/%m/%d", tz="") | 
+                   strptime(hpcData$Date, "%d/%m/%Y", tz="")==
+                   strptime("2007/02/02", "%Y/%m/%d", tz=""))
+    
+    write.table(workData,"WD.txt",row.names=FALSE)
+}  
+####################################################################################
+# add a POSIXct column, to the workData table 
+###################################################################################
+
+workData[, "PctDT"] <- 
+    as.POSIXct(strptime(paste(workData[, "Date"], 
+                              workData[, "Time"], sep=" "), "%d/%m/%Y %H:%M:%S", tz="")) 
+
+
+##################################################################################
+# plot using USA local for time definition, to get the correct x-axis titles 
+##################################################################################
+
+## create png file 
+png(file="plot2.png", width = 480, height = 480)
+
+
+Sys.setlocale("LC_TIME", "English")
+
+#par(cex=0.5)
+plot(workData$PctDT, workData$Global_active_power, type="l", 
+        ylab="Global Active Power (killowatts)", xlab="") 
+ 
+##################################################################################
+
+dev.off() 
+ 
